3ac0b0e0388d301774987717e31b1f641bdc87e5
agalzh <128179038@sastra.ac.in>
2026-02-28 23:23:59 +0530

    chore: HC-4 Imported fonts, stylings were matched with the nurse build

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,4 +1,3 @@
-import { Feather } from '@expo/vector-icons'; // Using Feather icons
 import { Link, router } from 'expo-router'; // Using Link for navigation with Expo Router
 import React, { useState } from 'react';
 import {
@@ -27,9 +26,11 @@ import {
   FormControlErrorText,
   FormControlErrorIcon,
 } from '@/components/ui/form-control';
-import { AlertCircleIcon } from '@/components/ui/icon';
+import { AlertCircleIcon, LockIcon, MailIcon } from '@/components/ui/icon';
 import logoImage from '../assets/images/HC_logo.png';
 import { EyeIcon, EyeOffIcon } from '@/components/ui/icon';
+import { useFonts } from 'expo-font';
+import { VStack } from '@/components/ui/vstack';
 
 const SignInScreen = () => {
   // State for the input fields
@@ -76,7 +77,7 @@ const SignInScreen = () => {
 
     } catch (e: any) {
       // Set a user-friendly error message
-      const errorMessage = (e.response && e.response.data && (e.response.data.message || e.response.data.error)) || 
+      const errorMessage = (e.response && e.response.data && (e.response.data.message || e.response.data.error)) ||
         'Invalid credentials or network error.';
       setError(errorMessage);
       console.error(e); // Log the full error for debugging
@@ -86,121 +87,149 @@ const SignInScreen = () => {
     }
   };
 
+  const [fontsLoaded] = useFonts({
+    'Sen-Regular': require('../assets/fonts/Sen-Regular.ttf'),
+    'Sen-Bold': require('../assets/fonts/Sen-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) return null;
+
   return (
     <SafeAreaProvider>
       <SafeAreaView className='flex-1 bg-[#369BFF]/80'>
         <KeyboardAvoidingView
           behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-          keyboardVerticalOffset={Platform.OS === 'ios' ? 60 : 0}
-          style={{ flex: 1 }} // Use explicit style here instead of className
+        // keyboardVerticalOffset={Platform.OS === 'ios' ? 60 : 0}
+        // style={{ flex: 1 }} // Use explicit style here instead of className
         >
           <ScrollView
-            contentContainerStyle={{ flexGrow: 1, paddingBottom: 40 }} // Added paddingBottom for extra keyboard clearance
+            contentContainerStyle={{
+              flexGrow: 1,
+              // paddingBottom: 40 
+            }} // Added paddingBottom for extra keyboard clearance
             keyboardShouldPersistTaps="handled"
             showsVerticalScrollIndicator={false}
             bounces={false}
           >
-
-            {/* Top Section / Logo */}
-            <Box className='justify-center items-center py-20'>
-              <Box className="w-24 h-24 mb-4">
-                <Image
-                  source={logoImage}
-                  className="w-full h-full"
-                  alt="HomeCare Logo"
-                  resizeMode="contain"
-                />
-              </Box>
-              <Text
-                className='text-5xl font-bold text-white text-center'
-                style={{ fontFamily: Platform.OS === 'ios' ? 'Avenir Next' : 'sans-serif-condensed' }}
-              >
-                HomeCare
-              </Text>
-              <Text className='text-lg text-white/70 text-center mt-2'>
-                Welcome back, please sign in
-              </Text>
-            </Box>
-
-            {/* Form Section */}
-            <Box className='rounded-3xl h-fit bg-white px-8 py-10 mx-5'>
-              <FormControl isInvalid={!!error} isRequired>
-
-                {/* Email Field */}
-                <FormControlLabel>
-                  <FormControlLabelText className='text-gray-800 uppercase font-medium leading-10'>
-                    Email Address
-                  </FormControlLabelText>
-                </FormControlLabel>
-                <Input className="bg-gray-100 border-transparent rounded-2xl h-14 px-4 mb-4">
-                  <InputSlot>
-                    <Feather name="mail" size={18} color="#a0aec0" />
-                  </InputSlot>
-                  <InputField
-                    type="text"
-                    placeholder="example@gmail.com"
-                    value={email}
-                    onChangeText={setEmail}
-                    keyboardType="email-address"
-                    autoCapitalize="none"
-                    className='text-gray-800'
+            <VStack className='items-center flex-1 mx-5'>
+              {/* Top Section / Logo */}
+              <Box className='justify-center items-center py-20'>
+                <Box className="w-24 h-24 mb-4">
+                  <Image
+                    source={logoImage}
+                    className="w-full h-full"
+                    alt="HomeCare Logo"
+                    resizeMode="contain"
                   />
-                </Input>
-
-                {/* Password Input Field */}
-                <FormControlLabel>
-                  <FormControlLabelText className='text-gray-800 uppercase font-medium leading-10'>
-                    Password
-                  </FormControlLabelText>
-                </FormControlLabel>
-                <Input className="bg-gray-100 border-transparent rounded-2xl h-14 px-4">
-                  <InputSlot>
-                    <Feather name="lock" size={18} color="#a0aec0" />
-                  </InputSlot>
-                  <InputField
-                    type={showPassword ? 'text' : 'password'}
-                    placeholder="********"
-                    value={password}
-                    onChangeText={setPassword}
-                    secureTextEntry={!showPassword}
-                    className='text-gray-800'
-                  />
-                  <InputSlot className="pr-3" onPress={handleState}>
-                    <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
-                  </InputSlot>
-                </Input>
-
-                {/* Error Message */}
-                <FormControlError className="mt-2">
-                  <FormControlErrorIcon as={AlertCircleIcon} />
-                  <FormControlErrorText>{error}</FormControlErrorText>
-                </FormControlError>
-
-                {/* Submit Button */}
-                <Button
-                  className='bg-[#369BFF] h-14 rounded-2xl mt-8 shadow-lg'
-                  onPress={handleSignIn}
-                  isDisabled={loading}
-                >
-                  {loading ? (
-                    <ButtonSpinner color="white" />
-                  ) : (
-                    <ButtonText className='text-white font-bold text-lg'>Sign In</ButtonText>
-                  )}
-                </Button>
-
-                {/* Sign Up Link */}
-                <Box className='flex-row justify-center mt-8'>
-                  <Text className='text-black'>Don&apos;t have an account? </Text>
-                  <Link href="/sign-up" asChild>
-                    <Pressable>
-                      <Text className='text-[#369BFF] font-bold'>Sign Up</Text>
-                    </Pressable>
-                  </Link>
                 </Box>
+                <Text
+                  className='text-5xl font-bold text-white text-center'
+                  style={{ fontFamily: Platform.OS === 'ios' ? 'Avenir Next' : 'sans-serif-condensed' }}
+                >
+                  HomeCare
+                </Text>
+                <Text style={{ fontFamily: "Sen-Regular" }}
+                  className="text-[18px] text-white/70 mt-[20px] mb-[20px] w-full text-center"
+                >
+                  Welcome back, please sign in
+                </Text>
+              </Box>
 
-              </FormControl>
-            </Box>
+              {/* Form Section */}
+              <VStack space="lg" className="bg-white w-full mt-5 px-8 py-10 rounded-3xl items-center">
+                <FormControl size="lg" className="w-full text-red-500" isInvalid={!!error} isRequired>
+
+                  {/* Email Field */}
+                  <FormControlLabel>
+                    <FormControlLabelText
+                      style={{ fontFamily: "Sen-Regular" }}
+                      className="text-gray-800 uppercase leading-10"
+                    >
+                      Email Address
+                    </FormControlLabelText>
+                  </FormControlLabel>
+                  <Input className="my-1 rounded-xl h-14 bg-[#F0F5FA] pl-4 border-0" size="md">
+                    <InputSlot>
+                      <InputIcon as={MailIcon} />
+                    </InputSlot>
+                    <InputField
+                      type="text"
+                      placeholder="example@gmail.com"
+                      value={email}
+                      onChangeText={setEmail}
+                      keyboardType="email-address"
+                      autoCapitalize="none"
+                      className='text-gray-800'
+                    />
+                  </Input>
+
+                  {/* Password Input Field */}
+                  <FormControlLabel>
+                      <FormControlLabelText
+                        style={{ fontFamily: "Sen-Regular" }}
+                        className="text-gray-800 uppercase leading-10"
+                      >
+                        Password
+                      </FormControlLabelText>
+                  </FormControlLabel>
+                    <Input
+                      className="my-1 rounded-xl h-14 bg-[#F0F5FA] pl-4 pr-4 border-0"
+                      size="md"
+                    >
+                    <InputSlot>
+                      <InputIcon as={LockIcon} />
+                    </InputSlot>
+                    <InputField
+                      style={{ fontFamily: "Sen-Regular" }}
+                      type={showPassword ? 'text' : 'password'}
+                      placeholder="********"
+                      value={password}
+                      onChangeText={setPassword}
+                      secureTextEntry={!showPassword}
+                      className='text-gray-800'
+                    />
+                    <InputSlot onPress={handleState}>
+                      <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
+                    </InputSlot>
+                  </Input>
+
+                  {/* Error Message */}
+                  <FormControlError className="mt-2">
+                    <FormControlErrorIcon as={AlertCircleIcon} />
+                    <FormControlErrorText>{error}</FormControlErrorText>
+                  </FormControlError>
+
+                  {/* Submit Button */}
+                  <Button
+                    className="w-full bg-[#369BFF] rounded-xl h-14 mt-8"
+                    onPress={handleSignIn}
+                    isDisabled={loading}
+                  >
+                    {loading ? (
+                      <ButtonSpinner color="white" />
+                    ) : (
+                      <ButtonText 
+                      style={{ fontFamily: "Sen-Bold" }}
+                      className="text-white text-xl"
+                      >Sign In</ButtonText>
+                    )}
+                  </Button>
+
+                  {/* Sign Up Link */}
+                  <Box className='flex-row justify-center mt-8'>
+                    <Text style={{ fontFamily: "Sen-Regular" }} className='text-black'>Don&apos;t have an account? </Text>
+                    <Link href="/sign-up" asChild>
+                      <Pressable>
+                        <Text style={{ fontFamily: "Sen-Bold" }} className='text-[#369BFF]'>Sign Up</Text>
+                      </Pressable>
+                    </Link>
+                  </Box>
+
+                </FormControl>
+              </VStack>
+            </VStack>
+
+
           </ScrollView>
         </KeyboardAvoidingView>
       </SafeAreaView>
